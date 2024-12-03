0ef9a97369b9d3495c0fc6bbc356019dbdc74e45
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-12-03 20:28:57 +0600

    Add user input script demonstrating argument handling and script name display

---
+++ userinput.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# read -p "Enter your name: " name
+# echo "Your name is $name"
+
+# read -sp "Enter your Password: " password
+# echo "Your password is $password"
+
+name=$0
+echo "The script name is $name"
+
+name=$1
+echo "The first argument is $name"
+
+name=$2
+echo "The second argument is $name"

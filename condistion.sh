4ef88b17d5570fdd4e6738c0ea910288a528fb20
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-12-07 22:45:14 +0600

    Refactor condition.sh to uncomment and enable user input checks for number, age, and marks

--- condistion.sh
+++ condistion.sh
@@ -1,32 +1,32 @@
 #! /usr/bin/bash
 
-# read -p "Enter a number: " number
-# if [ $number -gt 10 ]
-# then
-#     echo "The number is greater than 10"
-# else
-#     echo "The number is not greater than 10"
-# fi
+read -p "Enter a number: " number
+if [ $number -gt 10 ]
+then
+    echo "The number is greater than 10"
+else
+    echo "The number is not greater than 10"
+fi
 
-# read -p "Your Age: " age
-# if [ $age -gt 18 ] && [ $age -lt 30 ];
-# then
-#     echo "You are a young adult"
-# elif [ $age -gt 30 ] && [ $age -lt 40 ];
-# then
-#     echo "You are an adult"
-# else
-#     echo "You are a senior"
-# fi  
+read -p "Your Age: " age
+if [ $age -gt 18 ] && [ $age -lt 30 ];
+then
+    echo "You are a young adult"
+elif [ $age -gt 30 ] && [ $age -lt 40 ];
+then
+    echo "You are an adult"
+else
+    echo "You are a senior"
+fi  
 
-# read -p "Marks: " marks
-# if [[ $marks -gt 33  && $marks -lt 100 ]]; then
-#     echo "You are pass"
-# elif [[ $marks -lt 33 ]]; then
-#     echo "You are fail"
-# else
-#     echo "Invalid marks"
-# fi
+read -p "Marks: " marks
+if [[ $marks -gt 33  && $marks -lt 100 ]]; then
+    echo "You are pass"
+elif [[ $marks -lt 33 ]]; then
+    echo "You are fail"
+else
+    echo "Invalid marks"
+fi
 
 if [ -d devops ]; then
     echo "Directory exists"

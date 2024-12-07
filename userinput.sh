807f1b04f9acee70749db397ac0a992d8f2a9fe8
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-12-07 22:45:18 +0600

    Enable user input for name and password in userinput.sh

--- userinput.sh
+++ userinput.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-# read -p "Enter your name: " name
-# echo "Your name is $name"
+read -p "Enter your name: " name
+echo "Your name is $name"
 
-# read -sp "Enter your Password: " password
-# echo "Your password is $password"
+read -sp "Enter your Password: " password
+echo "Your password is $password"
 
 name=$0
 echo "The script name is $name"
